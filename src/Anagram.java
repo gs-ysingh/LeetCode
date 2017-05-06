01fbef26711b625ded71bce042354d6b6e13ed36
Yogesh Singh <ysingh@gainsight.com>
2017-05-06 20:00:00 +0530

    Anagram values

--- src/Anagram.java
+++ src/Anagram.java
@@ -8,7 +8,9 @@ public class Anagram {
     }
 
     private static boolean isAnagrm(String a, String b) {
+
         int [] count = new int[26];
+
         for (int i = 0; i < a.length(); i++) {
             count[a.charAt(i) - 'a']++;
             count[b.charAt(i) - 'a']--;
