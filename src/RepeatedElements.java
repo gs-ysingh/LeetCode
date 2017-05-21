39951d0e1460635e83f669efdd90c633cc444ee6
Yogesh Singh <ysingh@gainsight.com>
2017-05-21 20:00:00 +0530

    repeat elements

---
+++ src/RepeatedElements.java
@@ -0,0 +1,21 @@
+/**
+ * Created by YSingh on 23/05/17.
+ */
+public class RepeatedElements {
+    public static void main(String[] args) {
+        int arr[] = {1, 6, 3, 1, 3, 6, 6};
+        int arr_size = arr.length;
+        printRepeating(arr, arr_size);
+    }
+
+    private static void printRepeating(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            if(arr[Math.abs(arr[i])] >=0) {
+                arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+            }
+            else {
+                System.out.println(Math.abs(arr[i]));
+            }
+        }
+    }
+}
