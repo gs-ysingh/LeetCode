181a8c1051bf74576befbfb2d115dd89f1196b35
Yogesh Singh <ysingh@gainsight.com>
2017-05-22 20:00:00 +0530

    another way for repeated elements

--- src/RepeatedElements.java
+++ src/RepeatedElements.java
@@ -5,7 +5,20 @@ public class RepeatedElements {
     public static void main(String[] args) {
         int arr[] = {1, 6, 3, 1, 3, 6, 6};
         int arr_size = arr.length;
-        printRepeating(arr, arr_size);
+        //printRepeating(arr, arr_size);
+        printDuplicates(arr, arr_size);
+    }
+
+    private static void printDuplicates(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            arr[arr[i] % n] = arr[arr[i] % n] + n;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] / n > 1) {
+                System.out.println(i);
+            }
+        }
     }
 
     private static void printRepeating(int[] arr, int n) {
