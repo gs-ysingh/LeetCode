42682bbc3815b45f7f48620da10776a37b80375f
Yogesh Singh <ysingh@gainsight.com>
2017-05-30 23:53:55 +0530

    boggle

---
+++ src/Boggle.java
@@ -0,0 +1,19 @@
+/**
+ * Created by YSingh on 30/05/17.
+ */
+public class Boggle {
+    public static void main(String[] args) {
+        String [] dictionary = {"GEEKS", "FOR", "QUIZ", "GO"};
+
+        char [][] boggle = {
+                                    {'G','I','Z'},
+                                    {'U','E','K'},
+                                    {'Q','S','E'}
+                                };
+        findWords(boggle, dictionary);
+    }
+
+    private static void findWords(char[][] boggle, String[] dictionary) {
+
+    }
+}
